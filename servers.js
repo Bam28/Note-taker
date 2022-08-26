2818e349c7985a0de0fe9d09bda0fc4b36a62e5a
Luis Jimenez <luisjimenez@Luiss-Air.attlocal.net>
2022-08-25 22:43:15 -0500

    Added to somestuff

---
+++ servers.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const path =  require('path');
+const fs = require('fs');
+const notesAdded = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+
+const PORT = 3001
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notesAdded)
+});
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+    title,
+    text,
+    note_id: uuid(),
+    };
+
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+      // Add a new review
+      parsedNotes.push(newNote);
+
+      // Write updated reviews back to the file
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(parsedNotes),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated note!')
+      );
+    }
+  });
+
+  const response = {
+    status: 'success',
+    body: newNote,
+  };
+
+  console.log(response);
+  res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
+app.delete('/notes/:note_id', (req, res) => {
+
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+})
